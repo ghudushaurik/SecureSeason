b42a3307920bf96559443081151662be781c0778
ghudushaurik <konstantin.ghudushauri@gmail.com>
2023-03-04 18:11:38 +0400

    initial routes commit+db

--- easy-login/src/auth/auth.controller.ts
+++ easy-login/src/auth/auth.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Put } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/login')
+  async login(@Body() body: LoginDto) {
+    return;
+  }
+
+  @Post('/logout')
+  logout() {
+    return;
+  }
+
+  @Post('/register')
+  register() {
+    return;
+  }
+
+  @Put('/password')
+  changePassword() {
+    return;
+  }
+
+  @Put('/edit')
+  editUser() {
+    return;
+  }
+}
